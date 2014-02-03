f1f80098179f7f59e0a4d9a64e6497501a6f8c37
Bernhard Schussek <bschussek@gmail.com>
2014-02-03 18:49:58 +0100

    Added Webmozart\Puli\Puli class for retrieving the version and release date of the library

---
+++ src/Puli.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Puli package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\Puli;
+
+/**
+ * @since  %%NextVersion%%
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class Puli
+{
+    const VERSION = '@package_version@';
+
+    const RELEASE_DATE = '@release_date@';
+
+    private function __construct()
+    {
+    }
+}
