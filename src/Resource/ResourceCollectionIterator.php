900c0dc9102c1bc63e27bdaafa36b99057bf341a
Bernhard Schussek <bschussek@gmail.com>
2014-02-24 18:47:17 +0100

    Implemented ResourceCollectionIterator

---
+++ src/Resource/ResourceCollectionIterator.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the Puli package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\Puli\Resource;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class ResourceCollectionIterator implements \RecursiveIterator
+{
+    const CURRENT_AS_RESOURCE = 1;
+
+    const CURRENT_AS_PATH = 2;
+
+    const CURRENT_AS_REAL_PATH = 4;
+
+    const CURRENT_AS_NAME = 8;
+
+    const KEY_AS_PATH = 64;
+
+    /**
+     * @var ResourceInterface[]
+     */
+    protected $resources;
+
+    protected $cursor = 0;
+
+    protected $mode;
+
+    public function __construct(ResourceCollectionInterface $resources, $mode = null)
+    {
+        if (!($mode & (self::CURRENT_AS_PATH | self::CURRENT_AS_RESOURCE | self::CURRENT_AS_REAL_PATH | self::CURRENT_AS_NAME))) {
+            $mode |= self::CURRENT_AS_RESOURCE;
+        }
+
+        if (!($mode & (self::KEY_AS_PATH))) {
+            $mode |= self::KEY_AS_PATH;
+        }
+
+        $this->resources = $resources->toArray();
+        $this->mode = $mode;
+    }
+
+    public function current()
+    {
+        if ($this->mode & self::CURRENT_AS_RESOURCE) {
+            return $this->resources[$this->cursor];
+        }
+
+        if ($this->mode & self::CURRENT_AS_PATH) {
+            return $this->resources[$this->cursor]->getPath();
+        }
+
+        if ($this->mode & self::CURRENT_AS_REAL_PATH) {
+            return $this->resources[$this->cursor]->getRealPath();
+        }
+
+        return $this->resources[$this->cursor]->getName();
+    }
+
+    public function next()
+    {
+        ++$this->cursor;
+    }
+
+    public function key()
+    {
+        if ($this->mode & self::KEY_AS_PATH) {
+            return $this->resources[$this->cursor]->getPath();
+        }
+
+        return $this->cursor;
+    }
+
+    public function valid()
+    {
+        return $this->cursor < count($this->resources);
+    }
+
+    public function rewind()
+    {
+        $this->cursor = 0;
+    }
+
+    public function hasChildren()
+    {
+        return $this->resources[$this->cursor] instanceof DirectoryResourceInterface;
+    }
+
+    public function getChildren()
+    {
+        return new static($this->resources[$this->cursor]->all(), $this->mode);
+    }
+}
---
+++ tests/Resource/ResourceCollectionIteratorTest.php
@@ -0,0 +1,127 @@
+<?php
+
+/*
+ * This file is part of the Puli package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\Puli\Tests\Resource;
+
+use Webmozart\Puli\Repository\ResourceRepository;
+use Webmozart\Puli\Resource\ResourceCollectionIterator;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class ResourceCollectionIteratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefaultIteration()
+    {
+        $repo = new ResourceRepository();
+        $repo->add('/webmozart/puli', __DIR__.'/Fixtures');
+
+        $iterator = new ResourceCollectionIterator($repo->listDirectory('/'));
+
+        $recursiveIterator = new \RecursiveIteratorIterator(
+            $iterator,
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        $expected = array(
+            '/webmozart' => $repo->get('/webmozart'),
+            '/webmozart/puli' => $repo->get('/webmozart/puli'),
+            '/webmozart/puli/dir' => $repo->get('/webmozart/puli/dir'),
+            '/webmozart/puli/dir/nested' => $repo->get('/webmozart/puli/dir/nested'),
+            '/webmozart/puli/dir/nested/bar' => $repo->get('/webmozart/puli/dir/nested/bar'),
+            '/webmozart/puli/foo' => $repo->get('/webmozart/puli/foo'),
+        );
+
+        $this->assertSame($expected, iterator_to_array($recursiveIterator));
+    }
+
+    public function testCurrentAsPath()
+    {
+        $repo = new ResourceRepository();
+        $repo->add('/webmozart/puli', __DIR__.'/Fixtures');
+
+        $iterator = new ResourceCollectionIterator(
+            $repo->listDirectory('/'),
+            ResourceCollectionIterator::CURRENT_AS_PATH
+        );
+
+        $recursiveIterator = new \RecursiveIteratorIterator(
+            $iterator,
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        $expected = array(
+            '/webmozart' => '/webmozart',
+            '/webmozart/puli' => '/webmozart/puli',
+            '/webmozart/puli/dir' => '/webmozart/puli/dir',
+            '/webmozart/puli/dir/nested' => '/webmozart/puli/dir/nested',
+            '/webmozart/puli/dir/nested/bar' => '/webmozart/puli/dir/nested/bar',
+            '/webmozart/puli/foo' => '/webmozart/puli/foo',
+        );
+
+        $this->assertSame($expected, iterator_to_array($recursiveIterator));
+    }
+
+    public function testCurrentAsRealPath()
+    {
+        $repo = new ResourceRepository();
+        $repo->add('/webmozart/puli', __DIR__.'/Fixtures');
+
+        $iterator = new ResourceCollectionIterator(
+            $repo->listDirectory('/'),
+            ResourceCollectionIterator::CURRENT_AS_REAL_PATH
+        );
+
+        $recursiveIterator = new \RecursiveIteratorIterator(
+            $iterator,
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        $expected = array(
+            '/webmozart' => null,
+            '/webmozart/puli' => __DIR__.'/Fixtures',
+            '/webmozart/puli/dir' => __DIR__.'/Fixtures/dir',
+            '/webmozart/puli/dir/nested' => __DIR__.'/Fixtures/dir/nested',
+            '/webmozart/puli/dir/nested/bar' => __DIR__.'/Fixtures/dir/nested/bar',
+            '/webmozart/puli/foo' => __DIR__.'/Fixtures/foo',
+        );
+
+        $this->assertSame($expected, iterator_to_array($recursiveIterator));
+    }
+
+    public function testCurrentAsName()
+    {
+        $repo = new ResourceRepository();
+        $repo->add('/webmozart/puli', __DIR__.'/Fixtures');
+
+        $iterator = new ResourceCollectionIterator(
+            $repo->listDirectory('/'),
+            ResourceCollectionIterator::CURRENT_AS_NAME
+        );
+
+        $recursiveIterator = new \RecursiveIteratorIterator(
+            $iterator,
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        $expected = array(
+            '/webmozart' => 'webmozart',
+            '/webmozart/puli' => 'puli',
+            '/webmozart/puli/dir' => 'dir',
+            '/webmozart/puli/dir/nested' => 'nested',
+            '/webmozart/puli/dir/nested/bar' => 'bar',
+            '/webmozart/puli/foo' => 'foo',
+        );
+
+        $this->assertSame($expected, iterator_to_array($recursiveIterator));
+    }
+}
