7a36f66cbb8400681bf3f97dfc837902ccbbd382
Bernhard Schussek <bschussek@gmail.com>
2014-02-24 15:48:33 +0100

    Implemented PuliRewriteCssFilter

---
+++ src/Extension/Assetic/Asset/PuliAsset.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Puli package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\Puli\Extension\Assetic\Asset;
+
+use Assetic\Asset\FileAsset;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class PuliAsset extends FileAsset implements PuliAssetInterface
+{
+    public function __construct($path, $realPath, $filters = array(), array $vars = array())
+    {
+        parent::__construct($realPath, $filters, null, $path, $vars);
+    }
+}
