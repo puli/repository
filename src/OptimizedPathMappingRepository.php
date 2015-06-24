d6cc247d1618c841d7396ee758674c1c8ecc89bb
Titouan Galopin <galopintitouan@gmail.com>
2015-06-24 17:33:11 +0200

    Implement remove(), listChildren() and hasChildren() in optimized PathMapping repository

--- src/OptimizedPathMappingRepository.php
+++ src/OptimizedPathMappingRepository.php
@@ -15,7 +15,9 @@ use Puli\Repository\Api\EditableRepository;
 use Puli\Repository\Api\Resource\FilesystemResource;
 use Puli\Repository\Api\ResourceNotFoundException;
 use Puli\Repository\Api\UnsupportedResourceException;
+use Puli\Repository\Resource\Collection\FilesystemResourceCollection;
 use Webmozart\Assert\Assert;
+use Webmozart\Glob\Iterator\RegexFilterIterator;
 use Webmozart\KeyValueStore\Api\KeyValueStore;
 use Webmozart\PathUtil\Path;
 
@@ -101,7 +103,17 @@ class OptimizedPathMappingRepository implements EditableRepository
      */
     public function remove($query, $language = 'glob')
     {
-        // TODO
+        $resources = $this->find($query, $language);
+        $nbOfResources = count($this->store->keys());
+
+        // Run the assertion after find(), so that we know that $query is valid
+        Assert::notEq('', trim($query, '/'), 'The root directory cannot be removed.');
+
+        foreach ($resources as $resource) {
+            $this->removeResource($resource);
+        }
+
+        return $nbOfResources - count($this->store->keys());
     }
 
     /**
@@ -121,7 +133,10 @@ class OptimizedPathMappingRepository implements EditableRepository
      */
     public function listChildren($path)
     {
-        // TODO
+        $iterator = $this->getChildIterator($this->get($path));
+        $children = $this->store->getMultiple(iterator_to_array($iterator));
+
+        return new FilesystemResourceCollection($children);
     }
 
     /**
@@ -129,7 +144,10 @@ class OptimizedPathMappingRepository implements EditableRepository
      */
     public function hasChildren($path)
     {
-        // TODO
+        $iterator = $this->getChildIterator($this->get($path));
+        $iterator->rewind();
+
+        return $iterator->valid();
     }
 
 
@@ -139,14 +157,63 @@ class OptimizedPathMappingRepository implements EditableRepository
      */
     private function addResource($path, FilesystemResource $resource)
     {
+        // Don't modify resources attached to other repositories
+        if ($resource->isAttached()) {
+            $resource = clone $resource;
+        }
+
+        // Read children before attaching the resource to this repository
+        $children = $resource->listChildren();
+
+        $resource->attachTo($this, $path);
+
         // Add the resource before adding its children, so that the array
         // stays sorted
         $this->store->set($path, $resource);
 
         $basePath = '/' === $path ? $path : $path.'/';
 
-        foreach ($resource->listChildren() as $name => $child) {
+        foreach ($children as $name => $child) {
             $this->addResource($basePath.$name, $child);
         }
+    }
+
+
+    /**
+     * @param FilesystemResource $resource
+     */
+    private function removeResource(FilesystemResource $resource)
+    {
+        $path = $resource->getPath();
+
+        // Ignore non-existing resources
+        if (! $this->store->exists($path)) {
+            return;
+        }
+
+        // Recursively register directory contents
+        foreach ($this->getChildIterator($resource) as $child) {
+            $this->removeResource($child);
+        }
+
+        $this->store->remove($path);
+
+        // Detach from locator
+        $resource->detach($this);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    private function getChildIterator(FilesystemResource $resource)
+    {
+        $staticPrefix = rtrim($resource->getPath(), '/').'/';
+        $regExp = '~^'.preg_quote($staticPrefix, '~').'[^/]+$~';
+
+        return new RegexFilterIterator(
+            $regExp,
+            $staticPrefix,
+            new \ArrayIterator($this->store->keys())
+        );
     }
 }
